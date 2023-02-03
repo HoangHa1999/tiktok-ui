6e25e28e78b3ce2879a6a7143b3e3538acb71ba3
HoangHa <dh51806426@student.stu.edu.vn>
2023-02-03 16:33:45 +0700

    header

---
+++ src/components/Layouts/HeaderOnly/index.js
@@ -0,0 +1,13 @@
+import Header from '~/components/Layouts/components/Header';
+
+function HeaderOnly({ children }) {
+    return (
+        <>
+            <Header />
+            <div className="container">
+                <div className="content">{children}</div>
+            </div>
+        </>
+    );
+}
+export default HeaderOnly;
