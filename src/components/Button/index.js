d9ec392335c0c7a7448fff8f81155476ee8006d0
HoangHa <dh51806426@student.stu.edu.vn>
2023-02-18 15:41:58 +0700

    header ui phan 3

---
+++ src/components/Button/index.js
@@ -0,0 +1,57 @@
+import classNames from 'classnames/bind';
+import styles from './Button.module.scss';
+import { Link } from 'react-router-dom';
+
+const cx = classNames.bind(styles);
+
+function Button({
+    to,
+    href,
+    primary = false,
+    outline = false,
+    text = false,
+    rounded = false,
+    disabled = false,
+    small = false,
+    large = false,
+    leftIcon,
+    rightIcon,
+    children,
+    className,
+    onClick,
+    ...passProps
+}) {
+    let Comp = 'button';
+    const props = {
+        onClick,
+        ...passProps,
+    };
+
+    // Remove event listener when button is disabled
+    if (disabled) {
+        Object.keys(props).forEach((key) => {
+            if (key.startsWith('on') && typeof props[key] === 'function') {
+                delete props[key];
+            }
+        });
+    }
+
+    if (to) {
+        props.to = to;
+        Comp = Link;
+    } else if (href) {
+        props.href = href;
+        Comp = 'a';
+    }
+
+    const classes = cx('wrapper', { primary, outline, text, rounded, disabled, small, large, [className]: className });
+    return (
+        <Comp className={classes} {...props}>
+            {leftIcon && <span className={cx('left-icon')}>{leftIcon}</span>}
+            <span>{children}</span>
+            {rightIcon && <span className={cx('right-icon')}>{rightIcon}</span>}
+        </Comp>
+    );
+}
+
+export default Button;
